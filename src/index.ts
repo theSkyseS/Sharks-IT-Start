5375160a8f73bb9e9d9a1393ab35b6748bdfffad
theSkyseS <--add>
2023-03-12 16:44:33 +0600

    feat: added filmsRouter to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import dotenv = require("dotenv");
 import { Application } from "./framework/Application";
 import { genresRouter } from "./routes/genresRouter";
+import { filmsRouter } from "./routes/filmsRouter";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = new Application();
 
 app.addRouter(genresRouter);
+app.addRouter(filmsRouter);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
